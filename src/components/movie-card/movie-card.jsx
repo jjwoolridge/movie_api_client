93b314239754dc54e1bd1b9e4a61aa6bd2ffaf02
jjwoolridge <jyoti.pferd@gmail.com>
2021-05-01 17:12:08 +0200

    update to source files
    Added component life cycle react components and scss files

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+//import .scss for view'
+import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
   render () {
@@ -6,3 +9,21 @@ export class MovieCard extends React.Component {
     return <div className = "movie-card" onClick={() => {onMovieClick(movieData);}}>{movieData.Title}</div>;
   }
 }
+
+MovieCard.propTypes = {
+  movieData: PropTypes.shape({ 
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Birth: PropTypes.string
+    }), 
+    Year: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
